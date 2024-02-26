f39433a086edba6c03a4989c402353d4a8e77258
KirGorbunov <KirGorbunov@gmail.com>
2024-02-26 20:07:31 +0300

    change ordering in Dockerfile

--- docker_compose/simple_project/movies_admin/run_uwsgi.sh
+++ docker_compose/simple_project/movies_admin/run_uwsgi.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-python manage.py migrate
 python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
 python /opt/sqlite_to_postgres/load_data.py
