15657cb674ec28e68e784a33dbd352e7a54a73bf
KirGorbunov <KirGorbunov@gmail.com>
2024-02-29 21:09:47 +0300

    delete sqlite to postgresql script from run_uwsgi.sh

--- docker_compose/movies/movies_admin/run_uwsgi.sh
+++ docker_compose/movies/movies_admin/run_uwsgi.sh
@@ -6,6 +6,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
-python /opt/sqlite_to_postgres/load_data.py
 
 uwsgi --strict --ini /opt/app/uwsgi.ini
