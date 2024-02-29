97717a952884a9f1deb9e987ee4185e845388bae
KirGorbunov <KirGorbunov@gmail.com>
2024-02-29 22:48:16 +0300

    finish correction of comments

--- django_api/movies_admin/run_uwsgi.sh
+++ django_api/movies_admin/run_uwsgi.sh
@@ -5,6 +5,6 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-#python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput
 
 uwsgi --strict --ini /opt/app/uwsgi.ini
